6302b35565981f4242254b4454186a90c04cf0e8
damwon <dsw00513@naver.com>
2021-08-09 23:38:39 +0900

    add 12.정렬 11650번

---
+++ BOJ/12.정렬/11650.js
@@ -0,0 +1,26 @@
+const readline = require('readline')
+ 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let input = []
+
+rl.on("line", function(line){
+    input.push(line.split(' '))
+}).on("close", function(){
+    let T = input.shift()
+    let nums = input
+    let answer = ''
+    nums.sort((a, b) => { 
+        if (a[0] === b[0]) {
+            return a[1] - b[1] 
+        }    
+        return a[0] - b[0]
+    })
+    .forEach(input => {
+        answer += `${input[0]} ${input[1]}\n`
+    })
+    console.log(answer)
+})
