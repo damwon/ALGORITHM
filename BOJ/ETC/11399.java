4b482b6517556aa465b0aaf16b6694fa56d2d57b
damwon <dsw00513@naver.com>
2021-09-08 23:47:06 +0900

    add BOJ 11399번

---
+++ BOJ/ETC/11399.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] people = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i=0; i<N; i++) {
+			people[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(people);
+		int temp = 0;
+		int sum = 0;
+		for (int i=0; i<N; i++) {
+			temp += people[i];
+			sum += temp;
+		}
+		System.out.println(sum);
+	}
+}
