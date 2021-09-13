2cf54bb06aed86419a63788602bea2fb26cd7b8b
damwon <dsw00513@naver.com>
2021-09-13 23:50:27 +0900

    add BOJ 2635번

---
+++ BOJ/ETC/2635.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class main {
+	static int N, max;
+	static ArrayList<Integer> result;
+	
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb=new StringBuilder();
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		for(int i=N/2; i<=N; i++) {
+			check(i);
+		}
+		
+		sb.append(max+"\n");
+		for(int i : result) {
+			sb.append(i+" ");
+		}
+		System.out.print(sb);
+		sc.close();
+	}
+	private static void check(int i) {
+		ArrayList<Integer> a=new ArrayList<Integer>();
+		a.add(N);
+		
+		int now = N;
+		int pre = i;
+		
+		while (true) {
+			if (now<0) {
+				break;
+			}
+			int temp = now;
+			if (pre >= 0) {
+				a.add(pre);
+			}
+			
+			now = pre;
+			pre = temp - now;
+		}
+		if(max<a.size()) {
+			max = a.size();
+			result = a;
+		}
+	}
+}
